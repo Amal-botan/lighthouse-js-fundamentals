6989b0851675995cc16ef0d23b966d85ede8cf7b
Amal Botan <amalbotan254@gmail.com>
2021-11-03 08:57:54 +0000

    Practising with simple functions

---
+++ hello.js
@@ -0,0 +1,25 @@
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+
+function hey() {
+  console.log("Hello, world");
+}
+
+
+
+
+const hi =  hey => {
+  console.log("Hello, world");
+}
+
+
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Amal"); 
